0f57a7fe996c93088fb548bf4d86b6e874982de4
Will O'Shaughnessy <willisthebest2006@gmail.com>
2020-07-29 22:42:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -130,16 +130,16 @@ bot.on('messageReactionAdd', async (reaction, user) => {
         reaction.message.guild.members.cache.find(member => user).roles.add(reaction.message.guild.roles.cache.find(role => role.name === "Verified"));
         user.send("You have been verified in my server! Please remember to be respectful and kind at all times! Other than that, do what you like!");
     }
-    if(reaction === ':one:' && reaction.message.id === '738116196729225226'){
-        reaction.message.member.roles.add(reaction.message.guild.roles.cache.find(role => role.name === "13-14"));
+    if(reaction === '1️⃣' && reaction.message.id === '738116196729225226'){
+        reaction.message.guild.members.cache.find(member => user).roles.add(reaction.message.guild.roles.cache.find(role => role.name === "13-14"));
         user.send("You have the `13-14` role!");
     }
-    if(reaction === ':two:' && reaction.message.id === '738116196729225226'){
-        reaction.message.membver.roles.add(reaction.message.guild.roles.cache.find(role => role.name === "15-16"));
+    if(reaction === '2️⃣' && reaction.message.id === '738116196729225226'){
+        reaction.message.guild.members.cache.find(member => user).roles.add(reaction.message.guild.roles.cache.find(role => role.name === "15-16"));
         user.member.send("You have the `15-16` role!");
     }
-    if(reaction === ':three:' && reaction.message.id === '738116196729225226'){
-        reaction.message.member.roles.add(reaction.message.guild.roles.cache.find(role => role.name === "17-18"));
+    if(reaction === '3️⃣' && reaction.message.id === '738116196729225226'){
+        reaction.message.guild.members.cache.find(member => user).roles.add(reaction.message.guild.roles.cache.find(role => role.name === "17-18"));
         user.send("You have the `17-18` role!");
     }
 });
