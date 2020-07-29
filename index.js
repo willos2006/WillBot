594f8e3eb11c2a05e51f48737f56ca0461363b62
Will O'Shaughnessy <willisthebest2006@gmail.com>
2020-07-29 21:52:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -117,21 +117,21 @@ function chooseTruth(){
 
 bot.on('messageReactionAdd', async (reaction, user) => {
     console.log("hi");
-    memberWhoReacted = bot.guild.members.find(member => member.id === user.id);
+    fetch(reaction.channel);
     if(reaction.message.id === '738112386279669792'){
         console.log("hi");
-        memberWhoReacted.addRole(reaction.message.guild.roles.find(role => role.name === "Verified"));
+        user.addRole(reaction.message.guild.roles.find(role => role.name === "Verified"));
         memberWhoReacted.send("You have been verified in my server! Please remember to be respectful and kind at all times! Other than that, do what you like!");
     }
     if(reaction === ':one:' && reaction.message.id === '738116196729225226'){
         console.log("one");
-        memberWhoReacted.addRole(reaction.message.guild.roles.find(role => role.name === "13-14"));
-        memberWhoReacted.send("You have the `13-14` role!");
+        user.addRole(reaction.message.guild.roles.find(role => role.name === "13-14"));
+        user.send("You have the `13-14` role!");
     }
     if(reaction === ':two:' && reaction.message.id === '738116196729225226'){
         console.log("two");
-        memberWhoReacted.addRole(reaction.message.guild.roles.find(role => role.name === "15-16"));
-        memberWhoReacted.send("You have the `15-16` role!");
+        user.addRole(reaction.message.guild.roles.find(role => role.name === "15-16"));
+        user.send("You have the `15-16` role!");
     }
     if(reaction === ':three:' && reaction.message.id === '738116196729225226'){
         console.log("three");
