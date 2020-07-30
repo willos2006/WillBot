945d16987d05f930ab6df4f658fa87419c8e63c5
Will O'Shaughnessy <willisthebest2006@gmail.com>
2020-07-30 21:52:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ bot.on('messageReactionAdd', async (reaction, user) => {
 		reaction.message.guild.members.cache.find(member => member.id === user.id).roles.add(reaction.message.guild.roles.cache.find(role => role.name === "Verified"));
 		user.send("You have been verified in my server! Please remember to be respectful and kind at all times! Other than that, do what you like!");
 	}
-	if(message.message.id === '738116196729225226'){
+	if(reaction.message.id === '738116196729225226'){
 		if(reaction.message.guild.members.cache.find(member => member.id === user.id).roles.cache.find(role => role.name === "11-14")){
 			await reaction.message.guild.members.cache.find(member => member.id == user.id).roles.remove(reaction.message.guild.roles.cache.find(role => role.name === "11-14"));
 		}
