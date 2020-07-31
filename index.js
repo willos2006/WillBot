b8e6076c5d372cf6f5c5d08f1eea6c0a71f96cf0
Will O'Shaughnessy <willisthebest2006@gmail.com>
2020-07-31 13:59:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -354,6 +354,7 @@ bot.on('message', msg => {
 			});
 		}
 		else{
+			amount = int(amount);
 			if(amount >= 99){
 				msg.channel.messages.fetch({limit: 100}).then(messages => {
 					msg.channel.bulkDelete(messages);
