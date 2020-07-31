9d8a19d237419acfe8c3a0fd2dc5c714435e3677
Will O'Shaughnessy <willisthebest2006@gmail.com>
2020-07-31 15:16:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -373,7 +373,7 @@ bot.on('message', msg => {
 	}
 	if(msg.content.startsWith('-kick') && msg.member.roles.cache.find(role => role.name === 'Admins')){
 		let userToID = msg.content.slice(6);
-		let regex = /<((@!?\d+)|(:.+?:\d+))>/
+		let regex = /(?<=<!@)(.*\n?)(?=>)/
 		userTo = regex.exec(userToID);
 		console.log(userTo);
 	}
