48b1ddf3e414e6bf6a897bf4df66ebaea0a3c70c
Will O'Shaughnessy <willisthebest2006@gmail.com>
2020-08-02 17:20:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -47,8 +47,10 @@ bot.on('ready', () => {
 	let userOnline = guild.channels.cache.find(channel => channel.id === '739487262248796254');
 	let userTot = guild.channels.cache.find(channel => channel.id === '739487343668363344');
 	setInterval(function(){
-		var totalUsersOnline = guild.members.filter(member => member.presence.status === 'online').array.length;
-		userOnline.setName("Users Online: " + totalUsersOnline);
+		guild.members.fetch().then(m => {
+			var totalUsersOnline = m.filter(member => member.presence.status === 'online').array.length;
+			userOnline.setName("Users Online: " + totalUsersOnline);
+		});
 	}, 1000);
 });
 		
