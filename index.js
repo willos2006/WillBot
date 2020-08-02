bc24b6f82fd5a85c0dd1dee920edcc0207955dc7
Will O'Shaughnessy <willisthebest2006@gmail.com>
2020-08-02 16:41:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ bot.on('ready', () => {
 	setInterval(function(){
 		var totalUsersOnline;
 		guild.members.fetch().then(m => {
-			var totalUsersOnline = m.filter(member => member.presence.status === 'online').size;
+			var totalUsersOnline = parseInt(m.filter(member => member.presence.status === 'online').size, 10);
 		});
 		userOnline.setName("Users Online: " + totalUsersOnline);
 	}, 100);
