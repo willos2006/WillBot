9f31744b551ef15b0f212f8657189c51603b72b4
Will O'Shaughnessy <willisthebest2006@gmail.com>
2020-08-02 18:32:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const TOKEN = process.env.TOKEN;
 
 bot.login(TOKEN);
 
-bot.on('ready', async() => {
+bot.on('ready', async function() {
 	console.info(`Logged in as ${bot.user.tag}!`);
 	bot.user.setStatus('available')
 	bot.user.setActivity('with the server!', {
