406ea826339ad5ffe8039d755df73cd39bbf25fc
Will O'Shaughnessy <willisthebest2006@gmail.com>
2020-08-02 17:17:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ bot.on('ready', () => {
 	setInterval(function(){
 		var totalUsersOnline = m.filter(member => member.presence.status === 'online').array.length;
 		userOnline.setName("Users Online: " + totalUsersOnline);
-	}, 100);
+	}, 1000);
 });
 		
 		
