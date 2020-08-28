459539dfef32b3b4277b637d3a19d2d9e522ec41
Will O'Shaughnessy <willisthebest2006@gmail.com>
2020-08-28 21:19:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client({partials: ['MESSAGE', 'CHANNEL', 'REACTION']});
 const TOKEN = process.env.TOKEN;
-var tickets = require('tickets.json')
 bot.login(TOKEN);
 
 bot.on('ready', () => {
