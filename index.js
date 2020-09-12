6271fd22f48c282ffd7c8d4d95db42e90e309c17
Will O'Shaughnessy <willisthebest2006@gmail.com>
2020-09-12 15:59:07 +0100

    added buying things

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 ﻿//require('dotenv').config(); 
 const Discord = require('discord.js');
+const { json } = require('express');
 const bot = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 const TOKEN = process.env.TOKEN;
 const keepAlive = require('./server');
@@ -605,7 +606,8 @@ bot.on('message', msg => {
           var json = {
             "userID": msg.author.id,
             "lastSeen": 0,
-            "money": 0
+            "money": 0,
+            "inv": []
           }
           stuff.push(json);
           stuff = JSON.stringify(stuff);
@@ -642,7 +644,8 @@ bot.on('message', msg => {
           var json = {
             "userID": msg.author.id,
             "lastSeen": 0,
-            "money": 0
+            "money": 0,
+            "inv": []
           }
           stuff.push(json);
           stuff = JSON.stringify(stuff);
@@ -660,7 +663,8 @@ bot.on('message', msg => {
               var json = {
                 "userID": m.userID,
                 "lastSeen": m.lastSeen,
-                "money": currentMoney + 100
+                "money": currentMoney + 100,
+                "inv": []
               };
               currentMoney += 100;
               stuff.push(json);
@@ -698,8 +702,84 @@ bot.on('message', msg => {
         else{
           shop = shop.charAt(0).toUpperCase() + shop.slice(1);
           embed.setTitle(`${shop} Shop Stock`);
-          embed.setDescription(list);
+          embed.setDescription(list + "\n *remember you can use `-buy [item]` to purchase something*");
           msg.channel.send({embed});
+        }
+      }, 50);
+    }
+    if(msg.content.toLowerCase().startsWith("-buy")){
+      var item = msg.content.toLowerCase().slice(5);
+      fs = require('fs');
+      var isAble = true;
+      fs.readFile('shop.json', 'utf8', function (err, data){
+        var count = 0;
+        var money = 0;
+        stuff = JSON.parse(data);
+        stuff.forEach(m => {
+          if(m.userID == msg.author.id){
+            count += 1;
+            money = m.money;
+          }
+        });
+        if(count == 0){
+          isAble == false;
+        }
+      });
+      setTimeout(function(){
+        if(!isAble){
+          embed.setTitle("Error");
+          embed.setDescription("Sorry, you do not have the funds to purchase this item.");
+        }
+        else{
+          var price = 0;
+          var id = 0;
+          var canContinue = true;
+          fs.readFile('shopInventory', 'utf8', function (err, data){
+            stuff = JSON.parse(data);
+            var count = 0;
+            stuff.forEach(m => {
+              if(m.name == item){
+                count += 1;
+                price = m.price;
+                id = m.id;
+              }
+            });
+            if(count == 0){
+              canContinue = false;
+              embed.setTitle("Error");
+              embed.setDescription(`No such item: ${item}`);
+              msg.channel.send({embed});
+            }
+          });
+          setTimeout(function(){
+            if(canContinue){
+              if(price >= money){
+                fs.readFile('shop.json', 'utf8', function (err,data){
+                  stuff = JSON.parse(data);
+                  index = stuff.findIndex(x => x.userID == msg.author.id);
+                  json = {
+                    "userID": msg.author.id,
+                    "lastSeen": stuff[index].lastSeen,
+                    "money": stuff[index].money -= price,
+                    "inv": stuff[index].inv
+                  }
+                  json.inv.push(id);
+                  stuff.splice(index, 1);
+                  stuff.push(json);
+                  stuff = JSON.stringify(stuff);
+                  fs.writeFile('shop.json', 'utf8', stuff);
+                  embed.setTitle("Bought item");
+                  embed.setDescription(`Successfully bought ${item}.`);
+                  msg.channel.send({embed});
+                });
+              }
+              else{
+                embed.setTitle("Error");
+                embed.setDescription(`Sorry, you do not have the funds to purchase this item.`);
+                msg.channel.send({embed});
+              }
+            }
+          }, 20);
         }
       }, 50);
     }
