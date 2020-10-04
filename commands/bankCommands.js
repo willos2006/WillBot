e8c85bc34505f8d545f45ad07a4c25f45ed6f52c
Will O'Shaughnessy <willisthebest2006@gmail.com>
2020-10-04 12:30:32 +0100

    added discord-side bank transfers

--- commands/bankCommands.js
+++ commands/bankCommands.js
@@ -6,14 +6,91 @@ module.exports = async (bot, config) => {
 
     bot.on('message', async msg => {
         embed.setFooter(msg.author.tag);
-        if(msg.content.toLowerCase().startsWith(prefix + 'onlinebanking')){
+        if (msg.content.toLowerCase().startsWith(prefix + 'onlinebanking')) {
             var username = encodeURIComponent(msg.author.tag.replace(" ", "%20"));
             embed.setTitle("Bank Statement");
-            embed.setDescription("Click on the link below to have access to your online banking:\n https://willbot.willos2006.repl.co/bankstate?id="+msg.author.id+"&name="+username+"&fullList=false");
-            await msg.author.send({embed});
+            embed.setDescription("Click on the link below to have access to your online banking:\n https://willbot.willos2006.repl.co/bankstate?id=" + msg.author.id + "&name=" + username + "&fullList=false");
+            await msg.author.send({ embed });
             embed.setTitle("Sent DM")
             embed.setDescription("Please check your DM's for the link to your online banking.");
-            msg.channel.send({embed})
+            msg.channel.send({ embed })
+        }
+        if (msg.content.toLowerCase.startsWith("-dontateto") && settings.economyCommandsEnabled == true) {
+            var args = [];
+            args = msg.content.toLowerCase().split(" ");
+            var id = args[1];
+            const regex = RegExp('<!@(.*)>', 'g');
+            id = regex.exec(id)[1];
+            var amount = args[2];
+            var currentDate = new Date();
+            var date = currentDate.getDate() + "/" + (currentDate.getMonth() + 1) + "/" + currentDate.getFullYear();
+            var hours = currentDate.getHours();
+            if (hours.toString().length < 2) {
+                hours = 0 + hours.toString();
+            }
+            var mins = currentDate.getMinutes();
+            if (mins.toString().length < 2) {
+                mins = 0 + mins.toString();
+            }
+            var time = hours + ":" + mins;
+            if (id == '') {
+                embed.setTitle('Error');
+                embed.setDescription("Please enter a valid user");
+            }
+            else if (amount == "") {
+                embed.setTitle('Error');
+                embed.setDescription("Please enter a valid amount");
+            }
+            else {
+                var fs = require('fs');
+                fs.readFile('shop.json', 'utf8', function (err, data) {
+                    data = JSON.parse();
+                    var recIndex = data.findIndex(x => x.userID == id);
+                    var userIndex = data.findIndex(x => x.userID == msg.author.id);
+                    if (recIndex != -1) {
+                        if (data[index].money >= amount) {
+                            if (userIndex != -1) {
+                                data[recIndex].money += amount;
+                                data[recIndex].statement.push({
+                                    itemName: "Incoming online transfer from: " + msg.author.tag,
+                                    itemPrice: amount,
+                                    date: date,
+                                    time: time
+                                });
+                                data[userIndex].money -= amount;
+                                data[userIndex].statement.push({
+                                    itemName: "Outgoing online transfer to: " + msg.guild.members.cache.find(user => user.id == id).user.tag,
+                                    itemPrice: (amount * -1),
+                                    date: date,
+                                    time: time
+                                });
+                                data = JSON.stringify(data);
+                                fs.writeFile('shop.json', data, 'utf8', function(){});
+                                embed.setTitle("Transfer Complete");
+                                embed.setDescription(`Bank transfer successfully completed. Here is your reciept:`+"```"+`From: ${msg.author.tag}\nTo: ${msg.guild.members.cache.find(user => user.id == id).user.tag}\nAmount: £${amount.toFixed(2)}\nDate: ${date}\nTime: ${time}`+"```");
+                                msg.channel.send({embed});
+                            }
+                            else {
+                                embed.setTitle('Error');
+                                embed.setDescription("Not enough funds in your account to transfer £" + amount);
+                            }
+                        }
+                        else {
+                            embed.setTitle('Error');
+                            embed.setDescription("Not enough funds in your account to transfer £" + amount);
+                        }
+                    }
+                    else {
+                        embed.setTitle('Error');
+                        embed.setDescription("Please enter a valid user");
+                    }
+                });
+            }
+        }
+        else if (msg.content.toLowerCase.startsWith("-dontateto")) {
+            embed.setTitle("Economy Commands Disabled");
+            embed.setDescription("Sorry, but economy commands are currently disabled...");
+            msg.channel.send({ embed });
         }
     });
 }
